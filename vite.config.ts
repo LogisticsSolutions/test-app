432f90fa064dd07c04ee463ba7398e1a90613e61
Aziz Sydikov <asydikov@gmail.com>
2024-12-14 23:27:15 +0500

    add base dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'logisticssolutions.github.io',
   build: {
     outDir: 'docs',
     emptyOutDir: true, // also necessary
